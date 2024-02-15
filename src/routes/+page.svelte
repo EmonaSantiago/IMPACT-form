accac7426a5b1ab6ca98a070f9916ddc7d068ead
Emona Santiago <emonasantiago1207@gmail.com>
2024-02-15 15:48:58 +0100

    home page
    - routes pages fixed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,57 @@
-<script>
-    import Form from "./components/form.svelte";
-</script>
+<body>
+    <h1>IMPACT</h1>
+    <h3>Welkom bij ons <span>aanmeldingsformulier</span> voor een <span>positie bij Impact!</span> We zijn verheugd dat je interesse toont om deel uit te maken van ons team.</h3>
 
-<Form></Form>
+    
+    <ul>
+       <li><a class="button" href="src/routes/pages/creative/+page.svelte">Creative</a></li> 
+       <li><a class="button" href="src/routes/pages/crew/+page.svelte">Crew</a></li>
+        <li><a class="button" href="src/routes/pages/model/+page.svelte">Model</a></li>
+    </ul>
+
+    
+</body>
+
+<style>
+    body {
+        margin-top: 3em;
+        text-align: center;
+    }
+
+    h1 {
+        font-size: 110px;
+        color: #F8F053;
+    }
+    h3 {
+        color: #F8F053;
+        font-size: 16px;
+    }
+
+    span { 
+        color: white;
+    }
+
+    a {
+        text-decoration: none;
+        color: black;
+    }
+
+    li {
+        list-style-type: none;
+        width: 250px;
+        padding: 1em;
+        margin-bottom: 30px;  
+        background-color: #F8F053;
+        text-align: center;
+    }
+
+    ul {
+        padding: 0;
+        display: inline-block;
+        text-align: left;
+        margin-top: 5em;
+    }
+
+
+
+</style>
---
+++ src/routes/pages/creative/+page.svelte
@@ -0,0 +1,1 @@
+<h1>HI CREATIVE</h1>
